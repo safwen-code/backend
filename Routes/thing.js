8fd11c8f7bd0cc80cf22ea512c59d98cb17ded41
safwen-code <safwendjebbi1234@gmail.com>
2021-09-25 00:24:41 +0100

    create Models

--- Routes/thing.js
+++ Routes/thing.js
@@ -2,27 +2,28 @@ const express = require("express");
 const multer = require("../middelware/multer-config");
 const router = express.Router();
 const Thing = require("../Models/Thing");
-
+const fs = require("fs");
 //add doc
-router.post("/add", multer, (req, res, next) => {
+router.post("/add", multer, async (req, res, next) => {
   console.log(req.file);
   console.log(req.body);
   const { titel, description, price, userId } = req.body;
-
-  const thing = new Thing({
-    titel,
-    description,
-    userId,
-    price,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
-  });
-  console.log("thing", thing);
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
+  try {
+    const thing = new Thing({
+      titel,
+      description,
+      userId,
+      price,
+      imageUrl: `${req.protocol}://${req.get("host")}/images/${
+        req.file.filename
+      }`,
+    });
+    console.log("thing", thing);
+    await thing.save();
+    return res.status(201).json({ thing });
+  } catch (error) {
+    return res.status(500).json({ errorF });
+  }
 });
 
 //get all docs
@@ -49,33 +50,41 @@ router.get("/:id", async (req, res) => {
 });
 
 //update thing
-router.put("/updatedocs/:id", multer, (req, res) => {
+router.put("/updatedocs/:id", multer, async (req, res) => {
   // const { titel, description, price, userId } = req.body;
-  const thingObject = req.file
-    ? {
-        ...req.body,
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
-  Thing.updateOne({ id: req.params.id }, { ...thingObject, id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-  console.log(thingObject);
+  try {
+    const thingObject = req.file
+      ? {
+          ...req.body,
+          imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            req.file.filename
+          }`,
+        }
+      : { ...req.body };
+    const thingUpdated = await Thing.updateOne(
+      { id: req.params.id },
+      { ...thingObject, id: req.params.id }
+    );
+    console.log(thingObject);
+    return res.status(200).json({ thingObject });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 });
 
 //delete thing
-router.delete("/delete/:id", async (req, res) => {
+router.delete("/delete/:id", multer, async (req, res) => {
   const id = req.params.id;
   try {
-    const thingDeleted = await Thing.findOne({ id });
-    console.log(thingDeleted);
-    await thingDeleted.remove();
-    return res.status(200).json({ msg: `thin is deleted ${thingDeleted}` });
+    const thing = await Thing.findOne({ id });
+    const filename = thing.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${filename}`, async () => {
+      let thingdel = await Thing.deleteOne({ id });
+     
+    });
+    return res.status(200).json({ thing });
   } catch (error) {
     return res.status(500).json({ error });
-    console.log(msg.error);
   }
 });
 
